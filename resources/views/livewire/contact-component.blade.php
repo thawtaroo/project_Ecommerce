02bf0fa3569d30f7591e8e5d640c3faaa33f0fd2
TTO <myatthuta2019@gmail.com>
2023-07-15 11:01:00 +0630

    First Commit

---
+++ resources/views/livewire/contact-component.blade.php
@@ -0,0 +1,83 @@
+<div>
+    <main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><span>Contact us</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class=" main-content-area">
+					<div class="wrap-contacts ">
+						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+							<div class="contact-box contact-form">
+								<h2 class="box-title">Leave a Message</h2>
+								<form action="#" method="get" name="frm-contact">
+
+									<label for="name">Name<span>*</span></label>
+									<input type="text" value="" id="name" name="name" >
+
+									<label for="email">Email<span>*</span></label>
+									<input type="text" value="" id="email" name="email" >
+
+									<label for="phone">Number Phone</label>
+									<input type="text" value="" id="phone" name="phone" >
+
+									<label for="comment">Comment</label>
+									<textarea name="comment" id="comment"></textarea>
+
+									<input type="submit" name="ok" value="Submit" >
+									
+								</form>
+							</div>
+						</div>
+						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+							<div class="contact-box contact-info">
+								<div class="wrap-map">
+                                    <div>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d473.53863031261733!2d95.319786!3d18.28747!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c741ed2508c7ef%3A0xa47e8608bdc9bd5d!2sMyat%20ThuTa%20Mobile%20%26%20Electronic!5e0!3m2!1sen!2sus!4v1689349657448!5m2!1sen!2sus" width="500" height="263" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                    </iframe>
+                                    </div>
+								</div>
+								<h2 class="box-title">Contact Detail</h2>
+								<div class="wrap-icon-box">
+
+									<div class="icon-box-item">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+										<div class="right-info">
+											<b>Email</b>
+											<p>Support1@mtt.net</p>
+										</div>
+									</div>
+
+									<div class="icon-box-item">
+										<i class="fa fa-phone" aria-hidden="true"></i>
+										<div class="right-info">
+											<b>Phone</b>
+											<p>09-465-789-111</p>
+										</div>
+									</div>
+
+									<div class="icon-box-item">
+										<i class="fa fa-map-marker" aria-hidden="true"></i>
+										<div class="right-info">
+											<b>Mail Office</b>
+											<p>Sed ut perspiciatis unde omnis<br />Street Name, Los Angeles</p>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div><!--end main products area-->
+
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main>
+</div>
