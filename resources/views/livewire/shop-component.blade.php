699f47eebf69d607cc96156d83a1df1f832fe159
TTO <myatthuta2019@gmail.com>
2023-07-17 19:27:45 +0630

    finish price filter

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -125,7 +125,7 @@
                 </div><!-- brand widget-->
 
                 <div class="widget mercado-widget filter-widget price-filter">
-                    <h2 class="widget-title">Price</h2>
+                    <h2 class="widget-title">Price <span class="text-info">MMK {{ $min_price }} - MMK {{ $max_price }}</span></h2>
                     <div class="widget-content">
                         <div id="slider" wire:ignore>
                         </div>  
@@ -240,17 +240,22 @@
     <script>
         var slider = document.getElementById('slider');
         noUiSlider.create(slider,{
-            start : [1,1000],
+            start : [1,1000000],
             connect:true,
             range : {
                 'min' : 1,
-                'max' : 1000
+                'max' : 1000000
             },
             pips:{
                 mode:'steps',
                 stepped:true,
                 density:4
             }
+        });
+
+        slider.noUiSlider.on('update',function(value){
+            @this.set('min_price',value[0]);
+            @this.set('max_price',value[1]);
         });
     </script>
 @endpush
