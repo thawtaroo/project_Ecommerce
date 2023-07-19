ee42ac2cb7d1b682f514d8320cbe52ef189e6162
TTO <myatthuta2019@gmail.com>
2023-07-19 21:32:16 +0630

    create Cupon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -91,6 +91,9 @@
 														<a title="Sale Setting" href="{{ route('admin.sale') }}">Sale Setting</a>
 													</li>
 													<li class="menu-item">
+														<a title="All Coupon" href="{{ route('admin.coupons') }}">All Coupon</a>
+													</li>
+													<li class="menu-item">
 														<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 													</li>
 														<form id='logout-form' method="POST" action="{{ route('logout') }}">
