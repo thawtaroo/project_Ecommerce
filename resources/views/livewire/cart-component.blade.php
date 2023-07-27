5344395cd3cb8117bbd39c57027d257ade26d433
TTO <ttoo1@kmd.edu.mm>
2023-07-27 19:41:13 +0630

    Cart setting for checkout

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -11,6 +11,7 @@
 				</ul>
 			</div>
 			<div class=" main-content-area">
+				@if(Cart::instance('cart')->count() > 0)
 				<div class="wrap-iten-in-cart">
 					@if(Session::has('success_message'))
 							<div class="alert alert-success"> 
@@ -90,7 +91,7 @@
 								</div>
 							@endif
 						@endif
-						<a class="btn btn-checkout" href="checkout.html">Check out</a>
+						<a class="btn btn-checkout" href="#" wire:click.prevent="checkout">Check out</a>
 						<a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
 					</div>
 					<div class="update-clear">
@@ -98,6 +99,13 @@
 						<a class="btn btn-update" href="#">Update Shopping Cart</a>
 					</div>
 				</div>
+				@else
+				<div class="text-center" style="padding: 30px 0;">
+					<h1>Your Cart is empty!!</h1>
+					<p>Add items to it now</p>
+					<a href="/shop" class="btn btn-success">Shop Now</a>
+				</div>
+				@endif
 
 				<div class="wrap-iten-in-cart">
 					<h3 class="title-box" style="border-bottom:1px solid; padding-bottom:15px;">{{ Cart::instance('saveForLater')->count() }} item(s) Save For Later</h3>
--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class)->name('product.cart');
-Route::get('/checkout', CheckoutComponent::class);
+Route::get('/checkout', CheckoutComponent::class)->name('checkout');
 Route::get('/about',AboutComponent::class);
 Route::get('/contact',ContactComponent::class);
 Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
