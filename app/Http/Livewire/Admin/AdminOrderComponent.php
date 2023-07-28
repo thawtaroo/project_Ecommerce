0928bcc42820d186ee47a5d6bca1cabac172a1db
TTO <ttoo1@kmd.edu.mm>
2023-07-28 21:45:18 +0630

    make adminOrderComponent

---
+++ app/Http/Livewire/Admin/AdminOrderComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminOrderComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-order-component');
+    }
+}
