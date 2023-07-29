12e413edbf6d8c390e444cd9a8c2d77addf7392f
TTO <ttoo1@kmd.edu.mm>
2023-07-29 19:43:20 +0630

    p50 Show user Rating and Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,4 +9,9 @@ class Review extends Model
 {
     use HasFactory;
     protected $table = "reviews";
+
+    public function orderItem()
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
 }
