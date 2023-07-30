6c051107b524c16e5a369b9c7ff18e557b28a609
TTO <ttoo1@kmd.edu.mm>
2023-07-30 07:23:42 +0630

    p51 User Change Password

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\ThankyouComponent;
+use App\Http\Livewire\User\UserChangePasswordComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\User\UserOrderDetailsComponent;
 use App\Http\Livewire\User\UserOrdersComponent;
@@ -68,6 +69,7 @@ Route::middleware(['auth:sanctum','verified'])->group(function(){
     Route::get('/user/orders',UserOrdersComponent::class)->name('user.orders');
     Route::get('/user/orders/{order_id}',UserOrderDetailsComponent::class)->name('user.orderdetails');
     Route::get('user/review/{order_item_id}',UserReviewComponent::class)->name('user.review');
+    Route::get('/user/change-password',UserChangePasswordComponent::class)->name('user.changepassword');
 });
 
 //For Admin
