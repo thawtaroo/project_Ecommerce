293a498069173f997882366a1a491816deacfb78
tto <ttoo1@kmd.edu.mm>
2023-08-05 17:30:26 +0630

    p65 Show Processing Message

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -23,7 +23,7 @@
 				</ul>
 			</div>
 			<div class=" main-content-area">
-				<form wire:submit.prevent="placeOrder">
+				<form wire:submit.prevent="placeOrder" onsubmit="$('#processing').show();">
 					<div class="row">
 						<div class="col-md-12">
 							<div class="wrap-address-billing">
@@ -203,6 +203,14 @@
 							@if(Session::has('checkout'))
 								<p class="summary-info grand-total"><span>Grand Total</span> <span class="grand-total-price">${{ Session::get('checkout')['total'] }}</span></p>						
 							@endif
+
+								@if($errors->isEmpty())
+								<div wire:ignore id="processing" style="font-size:22px; margin-bottom:20px;padding-left:37px;color:green;display:none;">
+									<i class="fa fa-spinner fa-pluse fa-fw"></i>
+									<span>Processing....</span>
+								</div>
+								@endif
+
 							<button type="submit" class="btn btn-medium">Place order now</button>
 						</div>
 						<div class="summary-item shipping-method">
