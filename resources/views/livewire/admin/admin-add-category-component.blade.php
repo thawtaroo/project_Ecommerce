4bea74007a2dba8e9dab82d8835fe0d4f9a40dad
tto <ttoo1@kmd.edu.mm>
2023-08-05 19:11:46 +0630

    p67 Create Subcategories

--- resources/views/livewire/admin/admin-add-category-component.blade.php
+++ resources/views/livewire/admin/admin-add-category-component.blade.php
@@ -35,6 +35,18 @@
                             </div>
 
                             <div class="form-group">
+                                <label class="col-md-4 control-label">Parent Category</label>
+                                <div class="col-md-4">
+                                    <select class="form-control input-md" wire:model="category_id">
+                                        <option value="">None</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select> 
+                                </div>
+                            </div>
+
+                            <div class="form-group">
                                 <label class="col-md-4 control-label"></label>
                                 <div class="col-md-4">
                                     <button type="submit" class="btn btn-primary">Submit</button>
