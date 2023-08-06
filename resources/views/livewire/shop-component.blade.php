21c52678585cb654160bba857805bbd129b25d66
tto <ttoo1@kmd.edu.mm>
2023-08-06 09:26:52 +0630

    p71 show subcategories

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -136,8 +136,18 @@
                     <div class="widget-content">
                         <ul class="list-category">
                             @foreach ( $categories as $category )
-                                <li class="category-item">
+                                <li class="category-item {{ count($category->subCategories) > 0 ? 'has-child-cate':''}}">
                                     <a href="{{ route('product.category',['category_slug'=>$category->slug]) }}" class="cate-link">{{ $category->name }}</a>
+                                    @if (count($category->subcategories)>0)
+                                        <span class="toggle-control">+</span>
+                                        <ul class="sub-cate">
+                                            @foreach ($category->Subcategories as $category)
+                                                <li class="category-item">
+                                                    <a href="#" class="cat-link"><i class="fa fa-caret-right"></i>{{ $category->name }}</a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
                                 </li> 
                             @endforeach                            
                         </ul>
